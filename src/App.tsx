59857eb71d27dbc9c71a24191b81ccf4d08700b2
Rodolpho Reis <rodolphoreis@live.com>
2024-11-17 19:40:55 +0000

    feat: add react router dom and config react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Button } from "./components/ui/button.tsx";
 export function App() {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
+      <div className="flex justify-center items-center h-screen">
         <Button>Abrir</Button>
       </div>
     </>
---
+++ src/routes.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./pages/app/dashboard.tsx";
+import { SignIn } from "./pages/auth/sign-in.tsx";
+import { AppLayout } from "./pages/_layouts/app.tsx";
+import { AuthLayout } from "./pages/_layouts/auth.tsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+    ],
+  },
+]);
