3460dba3f517dcfaf10398e8defd6b88acf879e3
Rodolpho Reis <rodolphoreis@live.com>
2024-11-17 18:44:44 +0000

    chore: install and config shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button.tsx";
+
 export function App() {
   return (
     <>
       <div>
         <h1>Hello</h1>
+        <Button>Abrir</Button>
       </div>
     </>
   );
