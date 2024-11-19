2509c41519b86bc7441fc9b57d08aaad279b5c4c
Rodolpho Reis <rodolphoreis@live.com>
2024-11-19 17:13:29 +0000

    feat: add helmet in sign and dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "@/globals.css";
 import { router } from "./routes.tsx";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </StrictMode>
 );
