60df5d18210ccbec4a505b1e2242b7796e07a783
Rodolpho Reis <rodolphoreis@live.com>
2024-11-21 18:45:12 +0000

    chore: add dropdown-menu and theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import "@/globals.css";
 import { router } from "./routes.tsx";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </ThemeProvider>
   </StrictMode>
 );
