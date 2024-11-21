f6c1c37d3164c01ad7932f1c2e3828a6b529b77d
Rodolpho Reis <rodolphoreis@live.com>
2024-11-21 17:02:08 +0000

    feat: add sign-up route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./pages/app/dashboard.tsx";
 import { SignIn } from "./pages/auth/sign-in.tsx";
 import { AppLayout } from "./pages/_layouts/app.tsx";
 import { AuthLayout } from "./pages/_layouts/auth.tsx";
+import { SignUp } from "./pages/auth/sign-up.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
       {
         path: "/sign-in",
         element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
       },
     ],
   },
