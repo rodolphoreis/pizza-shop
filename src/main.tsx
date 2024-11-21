9f96839d7d09a10ebf8870ae569e443e4a127946
Rodolpho Reis <rodolphoreis@live.com>
2024-11-21 10:15:36 +0000

    feat: add Toaster from Sonner

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { RouterProvider } from "react-router-dom";
 import "@/globals.css";
 import { router } from "./routes.tsx";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   </StrictMode>
