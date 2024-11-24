b2a778063be7533aa568d92fb268317cb405ab2e
Rodolpho Reis <rodolphoreis@live.com>
2024-11-24 10:39:20 +0000

    feat: add new route

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { SignIn } from "./pages/auth/sign-in.tsx";
 import { AppLayout } from "./pages/_layouts/app.tsx";
 import { AuthLayout } from "./pages/_layouts/auth.tsx";
 import { SignUp } from "./pages/auth/sign-up.tsx";
+import Orders from "./pages/app/orders/orders.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Dashboard />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
       },
     ],
   },
