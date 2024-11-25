17ec2755ac76236f1e5c6a1cf4614a16f4d01cac
Rodolpho Reis <rodolphoreis@live.com>
2024-11-25 16:37:30 +0000

    feat: create forlder dashboard and change import

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/dashboard.tsx";
+
 import { SignIn } from "./pages/auth/sign-in.tsx";
 import { AppLayout } from "./pages/_layouts/app.tsx";
 import { AuthLayout } from "./pages/_layouts/auth.tsx";
 import { SignUp } from "./pages/auth/sign-up.tsx";
 import Orders from "./pages/app/orders/orders.tsx";
+import { Dashboard } from "./pages/app/dashboard/dashboard.tsx";
 
 export const router = createBrowserRouter([
   {
