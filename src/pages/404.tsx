2a2068e1a3c701d6c19a731dc59c05159a8077f7
Rodolpho Reis <rodolphoreis@live.com>
2024-11-27 17:40:31 +0000

    feat: create NotFound page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-2">
+      <h1 className="text-4xl font-bold">Página não encontrada</h1>
+      <p className="text-accent-foreground">
+        Voltar para o{" "}
+        <Link to="/" className="text-sky-600 dark:text-sky-400">
+          {" "}
+          Dashboard
+        </Link>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
