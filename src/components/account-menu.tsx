1dc4dcec1a772b0536d7b38867be04f74f2840bb
Rodolpho Reis <rodolphoreis@live.com>
2024-12-30 10:17:30 +0000

    feat: add isLoading

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -7,17 +7,18 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu.tsx";
 import { Button } from "./ui/button.tsx";
-import { Building, ChevronDown, LogOut } from "lucide-react";
+import { Building, ChevronDown, Divide, LogOut } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import { getProfileUser } from "@/api/get-profile-user.ts";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant.ts";
+import { Skeleton } from "./ui/skeleton.tsx";
 
 const AccountMenu = () => {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfileUser,
   });
-  const { data: managed } = useQuery({
+  const { data: managed, isLoading: isLoadingManaged } = useQuery({
     queryKey: ["managed-restaurant"],
     queryFn: getManagedRestaurant,
   });
@@ -28,16 +29,25 @@ const AccountMenu = () => {
           variant="outline"
           className="flex items-center gap-2 select-none"
         >
-          {managed?.name}
+          {isLoadingManaged ? <Skeleton className="h-4 w-40" /> : managed?.name}
           <ChevronDown className="w-4 h-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="e-56">
         <DropdownMenuLabel className="flex flex-col gap-1">
-          <span>{profile?.name}</span>
-          <span className="text-sm forn-normal text-muted">
-            {profile?.email}
-          </span>
+          {isLoadingProfile ? (
+            <div>
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-sm forn-normal text-muted">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
