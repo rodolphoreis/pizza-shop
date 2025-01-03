e87916b2947251c21fa9042ca749456a45bfd097
Rodolpho Reis <rodolphoreis@live.com>
2025-01-03 11:44:37 +0000

    feat: create signOut with useMatation

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,14 +8,18 @@ import {
 } from "./ui/dropdown-menu.tsx";
 import { Button } from "./ui/button.tsx";
 import { Building, ChevronDown, LogOut } from "lucide-react";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getProfileUser } from "@/api/get-profile-user.ts";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant.ts";
 import { Skeleton } from "./ui/skeleton.tsx";
 import { Dialog, DialogTrigger } from "./ui/dialog.tsx";
 import StoreProfireDialog from "./ui/store-profire-dialog.tsx";
+import { signOut } from "@/api/sign-out.ts";
+import { useNavigate } from "react-router-dom";
 
 const AccountMenu = () => {
+  const navigate = useNavigate();
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfileUser,
@@ -26,6 +30,14 @@ const AccountMenu = () => {
     queryFn: getManagedRestaurant,
     staleTime: Infinity,
   });
+
+  const { mutateAsync: signOutFn, isPending: isSignOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate("/sign-in", { replace: true });
+    },
+  });
+
   return (
     <Dialog>
       <DropdownMenu>
@@ -65,9 +77,14 @@ const AccountMenu = () => {
               <span>Perfil da loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem>
-            <LogOut className="text-rose-500 dark:text-rose-400" />
-            <span>Sair</span>
+          <DropdownMenuItem asChild disabled={isSignOut}>
+            <button
+              onClick={() => signOutFn()}
+              className="flex w-full items-center"
+            >
+              <LogOut className="text-rose-500 dark:text-rose-400" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
