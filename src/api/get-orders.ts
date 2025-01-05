c3c49d02b653800cccb0a3d50ec7fbb8b8f2ef29
Rodolpho Reis <rodolphoreis@live.com>
2025-01-05 16:43:07 +0000

    feat: create filters

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -2,6 +2,9 @@ import { api } from "@/lib/axios.ts";
 
 export interface GetOrdersQuery {
   pageIndex?: number | null;
+  orderId?: string | null;
+  customerName?: string | null;
+  status?: string | null;
 }
 
 export interface GetOrdersResponse {
@@ -21,10 +24,16 @@ export interface GetOrdersResponse {
 
 export async function getOrders({
   pageIndex,
+  orderId,
+  customerName,
+  status,
 }: GetOrdersQuery): Promise<GetOrdersResponse> {
   const response = await api.get<GetOrdersResponse>("/orders", {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   });
   return response.data;
