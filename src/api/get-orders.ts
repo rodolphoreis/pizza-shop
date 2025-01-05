2763dcc1c7958886c0ef094bfc9e27d1b537ab6e
Rodolpho Reis <rodolphoreis@live.com>
2025-01-05 11:41:43 +0000

    feat: create interface GetOrdersQuery

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,9 @@
 import { api } from "@/lib/axios.ts";
 
+export interface GetOrdersQuery {
+  pageIndex?: number | null;
+}
+
 export interface GetOrdersResponse {
   orders: {
     orderId: string;
@@ -15,10 +19,12 @@ export interface GetOrdersResponse {
   };
 }
 
-export async function getOrders() {
+export async function getOrders({
+  pageIndex,
+}: GetOrdersQuery): Promise<GetOrdersResponse> {
   const response = await api.get<GetOrdersResponse>("/orders", {
     params: {
-      pageIndex: 0,
+      pageIndex,
     },
   });
   return response.data;
