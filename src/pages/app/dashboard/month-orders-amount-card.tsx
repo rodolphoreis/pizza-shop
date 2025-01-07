3077c0b7dca1c0e2ee264f02354ce59daaae3d0b
Rodolpho Reis <rodolphoreis@live.com>
2025-01-07 17:50:02 +0000

    feat: integrate month orders amount data with useQuery and display in card component

--- src/pages/app/dashboard/month-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-orders-amount-card.tsx
@@ -1,12 +1,19 @@
+import { getMonthOrdersAmount } from "@/api/get-month-orders-amount.ts";
 import {
   Card,
   CardHeader,
   CardTitle,
   CardContent,
 } from "@/components/ui/card.tsx";
+import { useQuery } from "@tanstack/react-query";
 import { Utensils } from "lucide-react";
 
 const MonthOrdersAmountCard = () => {
+  const { data: monthOrdersAmountFn } = useQuery({
+    queryKey: ["metrics", "month-orders-amount"],
+    queryFn: getMonthOrdersAmount,
+  });
+
   return (
     <>
       <Card>
@@ -17,11 +24,31 @@ const MonthOrdersAmountCard = () => {
           <Utensils className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent className="space-y-1">
-          <span className="text-2xl font-bold tracking-tight"> 246</span>
-          <p className="text-sm text-muted-foreground">
-            <span className="text-emerald-500 dark:text-emerald-400"> +7%</span>{" "}
-            em relação ao mês passado
-          </p>
+          {monthOrdersAmountFn && (
+            <>
+              <span className="text-2xl font-bold tracking-tight">
+                {" "}
+                {monthOrdersAmountFn.amount}
+              </span>
+              <p className="text-sm text-muted-foreground">
+                {monthOrdersAmountFn.diffFromLastMonth >= 0 ? (
+                  <>
+                    <span className="text-emerald-500 dark:text-emerald-400">
+                      +{monthOrdersAmountFn.diffFromLastMonth}%
+                    </span>{" "}
+                    em relação ao mês passado
+                  </>
+                ) : (
+                  <>
+                    <span className="text-rose-500 dark:text-rose-400">
+                      -{monthOrdersAmountFn.diffFromLastMonth}%
+                    </span>{" "}
+                    em relação ao mês passado
+                  </>
+                )}
+              </p>
+            </>
+          )}
         </CardContent>
       </Card>
     </>
