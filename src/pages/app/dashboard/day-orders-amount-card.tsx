df247b5a5711daf3fe19abbfc787a815641367b5
Rodolpho Reis <rodolphoreis@live.com>
2025-01-07 17:49:25 +0000

    feat: integrate day orders amount with useQuery for dynamic data fetching

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,12 +1,19 @@
+import { getDayOrdersAmount } from "@/api/get-day-orders-amount.ts";
 import {
   Card,
   CardHeader,
   CardTitle,
   CardContent,
 } from "@/components/ui/card.tsx";
+import { useQuery } from "@tanstack/react-query";
 import { Utensils } from "lucide-react";
 
 const DayOrdersAmountCard = () => {
+  const { data: dayOrdersAmountFn } = useQuery({
+    queryKey: ["metrics", "day-orders-amount"],
+    queryFn: getDayOrdersAmount,
+  });
+
   return (
     <>
       <Card>
@@ -17,11 +24,30 @@ const DayOrdersAmountCard = () => {
           <Utensils className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent className="space-y-1">
-          <span className="text-2xl font-bold tracking-tight"> 12</span>
-          <p className="text-sm text-muted-foreground">
-            <span className="text-rose-500 dark:text-rose-400"> -3%</span> em
-            relação a ontem
-          </p>
+          {dayOrdersAmountFn && (
+            <>
+              <span className="text-2xl font-bold tracking-tight">
+                {dayOrdersAmountFn.amount.toLocaleString("pt-BR")}
+              </span>
+              <p className="text-sm text-muted-foreground">
+                {dayOrdersAmountFn.diffFromYesterday >= 0 ? (
+                  <>
+                    <span className="text-emerald-500 dark:text-emerald-400">
+                      +{dayOrdersAmountFn.diffFromYesterday}%
+                    </span>{" "}
+                    em relação a ontem
+                  </>
+                ) : (
+                  <>
+                    <span className="text-rose-500 dark:text-rose-400">
+                      -{dayOrdersAmountFn.diffFromYesterday}%
+                    </span>{" "}
+                    em relação a ontem
+                  </>
+                )}
+              </p>
+            </>
+          )}
         </CardContent>
       </Card>
     </>
