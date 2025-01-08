0ae19007dbd71d3e1cb0b26585c3914b9d8675ac
Rodolpho Reis <rodolphoreis@live.com>
2025-01-08 15:56:02 +0000

    feat: integrate month revenue data with useQuery and display formatted values

--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -1,12 +1,18 @@
+import { getMonthRevenue } from "@/api/get-month-revenue.ts";
 import {
   Card,
   CardHeader,
   CardTitle,
   CardContent,
 } from "@/components/ui/card.tsx";
+import { useQuery } from "@tanstack/react-query";
 import { DollarSign } from "lucide-react";
 
 const MonthRevenueCard = () => {
+  const { data: getMonthRevenueFn } = useQuery({
+    queryKey: ["metrics", "month-revenue"],
+    queryFn: getMonthRevenue,
+  });
   return (
     <>
       <Card>
@@ -17,11 +23,41 @@ const MonthRevenueCard = () => {
           <DollarSign className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent className="space-y-1">
-          <span className="text-2xl font-bold tracking-tight"> 345 €</span>
-          <p className="text-sm text-muted-foreground">
-            <span className="text-emerald-500 dark:text-emerald-400"> +2%</span>{" "}
-            em relação ao mês passado
-          </p>
+          <span className="text-2xl font-bold tracking-tight">
+            {" "}
+            {getMonthRevenueFn &&
+              (getMonthRevenueFn?.receipt / 100).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "EUR",
+              })}
+          </span>
+          {getMonthRevenueFn && (
+            <p className="text-sm text-muted-foreground">
+              {getMonthRevenueFn?.diffFromLastMonth < 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    {" "}
+                    {getMonthRevenueFn?.diffFromLastMonth.toLocaleString(
+                      "pt-BR"
+                    )}
+                    %
+                  </span>{" "}
+                  em relação ao mês passado
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {" "}
+                    {getMonthRevenueFn?.diffFromLastMonth.toLocaleString(
+                      "pt-BR"
+                    )}
+                    %
+                  </span>{" "}
+                  em relação ao mês passado
+                </>
+              )}
+            </p>
+          )}
         </CardContent>
       </Card>
     </>
