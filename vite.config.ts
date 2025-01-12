b0259891f0d6576acbe4f507a75e6eda1c3daeb6
Rodolpho Reis <rodolphoreis@live.com>
2025-01-12 12:32:32 +0000

    chore: config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
+
+import type { InlineConfig } from "vitest";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+  },
+} as UserConfig & {
+  test: InlineConfig;
 });
