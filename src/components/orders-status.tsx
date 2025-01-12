ff7dbc26d79112af98336485d4458f84687707d4
Rodolpho Reis <rodolphoreis@live.com>
2025-01-12 18:29:29 +0000

    feat: include data-testid

--- src/components/orders-status.tsx
+++ src/components/orders-status.tsx
@@ -10,7 +10,7 @@ interface OrderStatusProps {
 }
 
 const orderStatusMap: Record<OrderStatus, string> = {
-  pending: "Pedente",
+  pending: "Pendente",
   canceled: "Cancelado",
   processing: "Processando",
   delivering: "Entregando",
@@ -21,19 +21,31 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2 ">
       {status === "pending" && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        />
       )}
       {status === "canceled" && (
-        <span className="h-2 w-2 rounded-full bg-red-500" />
+        <span data-testid="badge" className="h-2 w-2 rounded-full bg-red-500" />
       )}
       {status === "processing" && (
-        <span className="h-2 w-2 rounded-full bg-yellow-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-yellow-500"
+        />
       )}
       {status === "delivering" && (
-        <span className="h-2 w-2 rounded-full bg-blue-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-blue-500"
+        />
       )}
       {status === "delivered" && (
-        <span className="h-2 w-2 rounded-full bg-green-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-green-500"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
