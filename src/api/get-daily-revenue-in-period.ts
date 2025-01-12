cd9fd2ace86932fa3c499c61844431fa56c8613b
Rodolpho Reis <rodolphoreis@live.com>
2025-01-12 11:55:40 +0000

    feat: add params

--- src/api/get-daily-revenue-in-period.ts
+++ src/api/get-daily-revenue-in-period.ts
@@ -1,13 +1,31 @@
 import { api } from "@/lib/axios.ts";
+import { format } from "date-fns";
+
+export interface GetDailyRevenueInPeriodQuery {
+  from?: Date;
+  to?: Date;
+}
 
 export type GetDailyRevenueInPeriodResponse = {
   data: string;
   receipt: number;
 }[];
 
-export async function getDailyRevenueInPeriod() {
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenueInPeriodQuery) {
+  const formattedFrom = from ? format(from, "yyyy-MM-dd") : undefined;
+  const formattedTo = to ? format(to, "yyyy-MM-dd") : undefined;
+
   const response = await api.get<GetDailyRevenueInPeriodResponse>(
-    "/metrics/daily-receipt-in-period"
+    "/metrics/daily-receipt-in-period",
+    {
+      params: {
+        from: formattedFrom,
+        to: formattedTo,
+      },
+    }
   );
 
   return response.data;
