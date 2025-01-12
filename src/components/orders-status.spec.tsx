6abdfa7db5c6535cd5794ab497cc074ed70be229
Rodolpho Reis <rodolphoreis@live.com>
2025-01-12 18:30:21 +0000

    feat: create test for order-status pending

--- src/components/orders-status.spec.tsx
+++ src/components/orders-status.spec.tsx
@@ -1,3 +1,15 @@
-test("1 + 1 equals 2", () => {
-  expect(1 + 1).toEqual(2);
+import { render } from "@testing-library/react";
+import { OrderStatus } from "./orders-status";
+import "@testing-library/jest-dom";
+
+describe("order-status", () => {
+  it("should display the right text when order status is pending", () => {
+    const wrapper = render(<OrderStatus status="pending" />);
+
+    const statusText = wrapper.getByText("Pendente");
+    const badgeElement = wrapper.getByTestId("badge");
+
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass("bg-slate-400");
+  });
 });
