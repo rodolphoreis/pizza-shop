d2fbfe8132d6a8692922fd9f36b15c0f72b54410
Rodolpho Reis <rodolphoreis@live.com>
2025-01-13 16:08:48 +0000

    test: ensure Pagination displays correct page and total count

---
+++ src/components/pagination.spec.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import Pagination from "./pagination";
+
+describe("Pagination", () => {
+  it("should display the right amount of pages and results", () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={() => {}}
+      />
+    );
+    expect(wrapper.getByText("Página 1 de 20")).toBeInTheDocument();
+    expect(wrapper.getByText("Total de 200 item(s)")).toBeInTheDocument();
+  });
+});
