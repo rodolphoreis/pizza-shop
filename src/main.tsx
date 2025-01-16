edc6929e3e43275cb89aedaf47d74bf74c9fb9d8
Rodolpho Reis <rodolphoreis@live.com>
2025-01-16 16:56:09 +0000

    chore: wrap application content with enableMSW for mock service support

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "@/globals.css";
-import { router } from "./routes.tsx";
+
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
-import { ThemeProvider } from "./components/theme/theme-provider.tsx";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/react-query.ts";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
-      <HelmetProvider>
-        <Helmet titleTemplate="%s | pizza.shop" />
-        <Toaster richColors />
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </HelmetProvider>
-    </ThemeProvider>
-  </StrictMode>
-);
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query.js";
+import { ThemeProvider } from "./components/theme/theme-provider.js";
+import { router } from "./routes.js";
+import { enableMSW } from "./api/mocks/index.js";
+
+enableMSW().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <HelmetProvider>
+          <Helmet titleTemplate="%s | pizza.shop" />
+          <Toaster richColors />
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        </HelmetProvider>
+      </ThemeProvider>
+    </StrictMode>
+  );
+});
