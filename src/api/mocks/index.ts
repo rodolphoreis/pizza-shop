f77d39aa6ef3731407572d326427ee21cb55d2dc
Rodolpho Reis <rodolphoreis@live.com>
2025-01-17 17:17:07 +0000

    refactor(mocks): include all API mocks and configure MSW worker

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,10 +1,33 @@
 /* eslint-disable prettier/prettier */
 import { env } from "@/pages/env";
 import { setupWorker } from "msw/browser";
+import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period-mock";
+import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
+import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders-amount-mock";
+import { getMonthOrdersAmountMock } from "./get-month-orders-amount-mock";
+import { getMonthRevenueMock } from "./get-month-revenue-mock";
+import { getPopularProductsMock } from "./get-popular-products-mock";
+import { registerRestaurantMock } from "./register-restaurant-mock";
 import { signInMock } from "./sign-in-mock";
+import { getOrdersMock } from "./get-orders-mock";
+import { getProfileMock } from "./get-profile-mock";
+import { getManagedRestaurantMock } from "./get-managed-restaurant-mock";
+import { updateProfileMock } from "./update-profile-mock";
 
-export const worker = setupWorker(signInMock);
-
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+  getOrdersMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock
+);
 export async function enableMSW() {
   if (env.MODE !== "test") {
     return;
